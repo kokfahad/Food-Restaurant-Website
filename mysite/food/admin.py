b363c960b60786b2393426d7d8ec5f8bf7634c24
Fahad Khan <1503059@student.ruet.ac.bd>
2021-06-23 16:20:01 +0600

    commit

---
+++ mysite/food/admin.py
@@ -0,0 +1,6 @@
+from food.models import Item
+from django.contrib import admin
+from .models import Item
+
+# Register your models here.
+admin.site.register(Item)
---
+++ mysite/food/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Item
+from .forms import ItemForm 
+from django.template import  loader
+# from .forms import ItemForm
+# Create your views here.
+def index(request):
+    item_list=Item.objects.all()
+   
+    context={
+         'item_list':item_list,
+    }
+    return render(request,'food/index.html',context)
+
+def item(request):
+    return HttpResponse('This is a item view')    
+
+def detail(request,item_id):
+    item=Item.objects.get(pk=item_id)
+    context= {
+        'item':item, 
+    }
+    return render(request,'food/detail.html',context)    
+
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request,'food/item-form.html',{'form':form} )    
+
+
+def update_item(request,id):
+    item= Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request,'food/item-form.html',{'form':form, 'item':item } ) 
+
+
+def delete_item(request,id):
+    item= Item.objects.get(id=id)
+
+    if request.method=='POST':
+        item.delete()
+        return redirect('food:index')
+
+    return render(request,'food/item-delete.html',{'item':item } ) 
